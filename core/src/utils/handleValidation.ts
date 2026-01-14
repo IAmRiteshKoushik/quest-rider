e16af8436ecd9e53477338539b4b052c7c4a8cdb
KiranRajeev-KV <kiranrajeevkv@gmail.com>
2026-01-15 03:06:01 +0530

    feat(core): add handleValidation

---
+++ core/src/utils/handleValidation.ts
@@ -0,0 +1,17 @@
+import { type ZodType } from "zod";
+import { throwError } from "./errorFunction";
+
+export function handleValidation<T>(schema: ZodType<T>, data: unknown, context: string): T | never {
+  const result = schema.safeParse(data);
+
+  if (!result.success) {
+    const errorJson: Record<string, string> = {};
+    result.error.issues.forEach((issue) => {
+      const field = issue.path.join('.');
+      errorJson[field] = issue.message;
+    });
+    return throwError(400, "Invalid request data", context, errorJson );
+  }
+
+  return result.data;
+}
